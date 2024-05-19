8fd69381218396156e4a84a563bf506d0d2c417b
Lakshmanakumar-K <lakshmanakumar15p624@gmail.com>
2024-05-19 18:39:19 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// let ele = document.createElement("p");
+// ele.innerHTML = "<p>this is paragraph</p>";
+// document.body.append(ele);
+
+
+// let d1 = document.createElement("div");
+// d1.setAttribute("class","container");
+// let d2 = document.createElement("div");
+// d2.setAttribute("class","row");
+// d1.append(d2);
+// let d3 = document.createElement("div");
+// d3.setAttribute("class","col");
+// d3.innerHTML = "this is col";
+// d2.append(d3);
+// document.body.append(d1);
+// d3.id = "ID1";
+// d3.style = "color:blue";
+
+function foo(){
+    let fn = document.getElementById("fn").value;
+    let mn = document.getElementById("mn").value;
+    let ln = document.getElementById("ln").value;
+    let email = document.getElementById("email").value;
+    let pn = document.getElementById("pn").value;
+    console.log(fn);
+    console.log(mn);
+    console.log(ln);
+    console.log(email);
+    console.log(pn);
+
+}
